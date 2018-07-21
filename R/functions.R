79ef0aad375c50d7a83b113e47b410e8941241e3
Brian O'Meara <omeara.brian@gmail.com>
2018-07-21 01:47:51 -0400

    more work on drake

--- R/functions.R
+++ R/functions.R
@@ -1,4 +1,4 @@
-gettrees <- function(clade="Hexapoda") {
+get_trees <- function(clade="Hexapoda") {
   treezip <- "http://files.opentreeoflife.org/synthesis/opentree9.1/opentree9.1_tree.tgz"
   download.file(treezip,destfile="opentree9.1_tree.tgz")
   untar("opentree9.1_tree.tgz")
@@ -8,4 +8,23 @@ gettrees <- function(clade="Hexapoda") {
   supertree.phy <- ape::extract.clade(supertree.phy, supertree.phy$node.label[which(grepl(clade, supertree.phy$node.label))])
   grafted.phy <- ape::extract.clade(grafted.phy, grafted.phy$node.label[which(grepl(clade, grafted.phy$node.label))])
   return(list(supertree.phy=supertree.phy, grafted.phy=grafted.phy ))
+}
+
+convert_tiplabels_to_genbank <- function(phy) {
+  try_names <- function(x) {
+    if(nchar(x)==0) {
+      return(x)
+    }
+    final.name <- x
+    ott.id <- gsub(".*_ott","",x)
+    new.final <- NA
+    try(new.final <- rotl::tax_name(rotl::taxonomy_taxon_info(ott.id))[[1]])
+    if(!is.na(new.final)) {
+      final.name <- new.final
+    }
+    return(final.name)
+  }
+  phy$tip.label <- sapply(phy$tip.label, try_names)
+  phy$node.label <- sapply(phy$node.label, try_names)
+  return(phy)
 }
--- R/plan.R
+++ R/plan.R
@@ -1,3 +1,11 @@
 plan <- drake_plan(
-  trees <- gettrees()
+  trees = get_trees(),
+  print(trees),
+  ot_supertree = convert_tiplabels_to_genbank(trees$supertree.phy),
+  ot_graftedtree = convert_tiplabels_to_genbank(trees$grafted.phy,
+  report = rmarkdown::render(
+    knitr_in("report.Rmd"),
+    output_file = file_out("report.html"),
+    quiet = TRUE
+  )
 )
