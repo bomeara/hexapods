1b3d8364176d4862a67df15117b6797d0d9feca5
Brian O'Meara <omeara.brian@gmail.com>
2018-08-04 16:04:30 -0400

    figures

--- make.R
+++ make.R
@@ -3,6 +3,9 @@ source("R/functions.R") # Load all the functions into your environment.
 source("R/plan.R")      # Build your workflow plan data frame.
 
 pkgconfig::set_config("drake::strings_in_dots" = "literals")
+knitr::opts_knit$set(root.dir = "docs")
+knitr::opts_knit$set(base.dir = "docs")
+
 #make(scholarplan)
 #make(plan)
 make(familyrun)
